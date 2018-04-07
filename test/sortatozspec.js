d46bc72753015892043bdcf2c9ff81ab56cc464c
Chandima Ranaweera <chan4lk@gmail.com>
2018-04-07 22:07:22 +0530

    coverage updated

--- test/sortatozspec.js
+++ test/sortatozspec.js
@@ -15,4 +15,28 @@ describe('sortatoz', () => {
     expect(sorted[1].name).to.be.equal('ranaweera');
    });
 
+   it('should return already sorted array by property', () => {
+    const arry = [
+      {name: 'chandima'},
+      {name: 'ranaweera'}
+    ];
+
+    const sorted = arry.sort(sortatoz('name'));
+    expect(sorted.length).to.be.equal(2);
+    expect(sorted[0].name).to.be.equal('chandima');
+    expect(sorted[1].name).to.be.equal('ranaweera');
+   });
+
+   it('should return same array when nested property have same value', () => {
+    const arry = [
+      {name: 'chandima', id : 1},
+      {name: 'chandima', id: 2}
+    ];
+
+    const sorted = arry.sort(sortatoz('name'));
+    expect(sorted.length).to.be.equal(2);
+    expect(sorted[0].id).to.be.equal(1);
+    expect(sorted[1].id).to.be.equal(2);
+   });
+
 });
